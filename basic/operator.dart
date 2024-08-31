79959936174f4f9f65e35e845c4dd58a8ccac303
Md Abdul Hai <abdulhai250620@gmail.com>
2024-09-01 00:32:04 +0600

    operator learning

---
+++ basic/operator.dart
@@ -0,0 +1,13 @@
+void main () {
+  int a =10;
+  int b=30;
+
+  print(a);
+  print(b);
+  print(a+b);
+  print(a-b);
+  print(a/b);
+  print(a*b);
+  print(a%b);
+
+}
---
+++ basic/variables.dart
@@ -0,0 +1,6 @@
+void main ( ) {
+  int roll = 21;
+  double cgpa= 3.5;
+  int result = cgpa.toInt() + roll;
+  print (result);
+}
